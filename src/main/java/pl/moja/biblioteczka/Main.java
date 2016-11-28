a594570d397f56292c56f1eb351caf87bbd0237f
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2016-11-28 20:28:17 +0100

    menu bar

--- src/main/java/pl/moja/biblioteczka/Main.java
+++ src/main/java/pl/moja/biblioteczka/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 /**
@@ -19,6 +20,7 @@ public class Main extends Application {
 
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/BorderPaneMain.fxml"));
+        Locale.setDefault(Locale.CANADA);
         ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
         loader.setResources(bundle);
         BorderPane borderPane = loader.load();
--- src/main/java/pl/moja/biblioteczka/controllers/MainController.java
+++ src/main/java/pl/moja/biblioteczka/controllers/MainController.java
@@ -1,12 +1,20 @@
 package pl.moja.biblioteczka.controllers;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ResourceBundle;
+
+import static javafx.application.Application.STYLESHEET_CASPIAN;
+import static javafx.application.Application.STYLESHEET_MODENA;
 
 /**
  * Created by ZacznijProgramowac on 09.11.2016.
@@ -37,4 +45,25 @@ public class MainController {
         borderPane.setCenter(parent);
     }
 
+    public void closeApplication(ActionEvent actionEvent) {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    public void setCaspian() {
+        Application.setUserAgentStylesheet(STYLESHEET_CASPIAN);
+    }
+
+    public void setModena() {
+        Application.setUserAgentStylesheet(STYLESHEET_MODENA);
+    }
+
+    public void setAlwaysOnTop(ActionEvent actionEvent) {
+       Stage stage = (Stage) borderPane.getScene().getWindow();
+        boolean value = ((CheckMenuItem) actionEvent.getSource()).selectedProperty().get();
+        stage.setAlwaysOnTop(value);
+    }
+
+    public void about() {
+    }
 }
