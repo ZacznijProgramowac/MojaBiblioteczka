a33dc7f4c9adc2f1e4efd3804a82efbbb01681bb
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2017-05-07 16:18:02 +0200

    tableview dla listy książek

---
+++ src/main/java/pl/moja/biblioteczka/controllers/ListBooksController.java
@@ -0,0 +1,17 @@
+package pl.moja.biblioteczka.controllers;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by ZacznijProgramowac.
+ * https://www.youtube.com/zacznijprogramowac
+ * http://zacznijprogramowac.net/
+ */
+public class ListBooksController {
+
+
+    @FXML
+    void initialize(){
+        System.out.println("Hello");
+    }
+}
